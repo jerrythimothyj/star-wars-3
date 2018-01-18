e7e23dac96129ec3728eb9adf7b7bbbcda8b5a19
Jerry Thimothy J <jerrythimothy@gmail.com>
2018-01-19 01:41:24 +0400

    More testers

---
+++ src/reducers/planet.reducers.test.js
@@ -0,0 +1,117 @@
+import reducer from './planet.reducers'
+import { SEARCH_PLANETS_REQUESTED, SEARCH_PLANETS, SEARCH_PLANETS_FAILED, SEARCH_PLANET_ALLOWED_REQUESTED, SEARCH_PLANET_ALLOWED, SEARCH_PLANET_ALLOWED_FAILED } from '../constants/planet.constants';
+
+describe('planet reducer', () => {
+  it('should return the initial state', () => {
+    expect(reducer(undefined, {})).toEqual(
+      {
+        planet: '',
+        planets: [],
+        isSearchAllowed: true
+      }
+    )
+  })
+
+  it('should handle SEARCH_PLANETS_REQUESTED', () => {
+    expect(
+      reducer({}, {
+        type: SEARCH_PLANETS_REQUESTED,
+      })
+    ).toEqual(
+        {}
+    )
+  });
+
+  it('should handle SEARCH_PLANETS', () => {
+    expect(
+      reducer({},
+        {
+            type: SEARCH_PLANETS,
+            planet: 'alderaan',
+            planets: [{
+                "name": "Alderaan",
+                "rotation_period": "24",
+                "orbital_period": "364",
+                "diameter": "12500",
+                "climate": "temperate",
+                "gravity": "1 standard",
+                "terrain": "grasslands, mountains",
+                "surface_water": "40",
+                "population": "2000000000",
+                "residents": ["https://swapi.co/api/people/5/", "https://swapi.co/api/people/68/", "https://swapi.co/api/people/81/"],
+                "films": ["https://swapi.co/api/films/6/", "https://swapi.co/api/films/1/"],
+                "created": "2014-12-10T11:35:48.479000Z",
+                "edited": "2014-12-20T20:58:18.420000Z",
+                "url": "https://swapi.co/api/planets/2/"
+            }]
+        }
+      )
+    ).toEqual(
+      {
+        planet: 'alderaan',
+        planets: [{
+            "name": "Alderaan",
+            "rotation_period": "24",
+            "orbital_period": "364",
+            "diameter": "12500",
+            "climate": "temperate",
+            "gravity": "1 standard",
+            "terrain": "grasslands, mountains",
+            "surface_water": "40",
+            "population": "2000000000",
+            "residents": ["https://swapi.co/api/people/5/", "https://swapi.co/api/people/68/", "https://swapi.co/api/people/81/"],
+            "films": ["https://swapi.co/api/films/6/", "https://swapi.co/api/films/1/"],
+            "created": "2014-12-10T11:35:48.479000Z",
+            "edited": "2014-12-20T20:58:18.420000Z",
+            "url": "https://swapi.co/api/planets/2/"
+        }]
+      }
+    )
+  })
+
+  it('should handle SEARCH_PLANETS_FAILED', () => {
+    expect(
+      reducer({}, {
+        type: SEARCH_PLANETS_FAILED,
+      })
+    ).toEqual(
+        {}
+    )
+  });
+
+  it('should handle SEARCH_PLANET_ALLOWED_REQUESTED', () => {
+    expect(
+      reducer({}, {
+        type: SEARCH_PLANET_ALLOWED_REQUESTED,
+      })
+    ).toEqual(
+        {}
+    )
+  });
+
+  it('should handle SEARCH_PLANET_ALLOWED', () => {
+    expect(
+      reducer({}, {
+        type: SEARCH_PLANET_ALLOWED,
+        isSearchAllowed: true
+      })
+    ).toEqual(
+        {
+            isSearchAllowed: true
+        }
+    )
+  });
+
+  it('should handle SEARCH_PLANET_ALLOWED_FAILED', () => {
+    expect(
+      reducer({}, {
+        type: SEARCH_PLANET_ALLOWED_FAILED,
+        isSearchAllowed: false
+      })
+    ).toEqual(
+        {
+            isSearchAllowed: false
+        }
+    )
+  });
+})
---
+++ src/reducers/user.reducers.test.js
@@ -0,0 +1,111 @@
+import reducer from './user.reducers'
+import { LOGIN_REQUESTED, LOGIN, LOGOUT_REQUESTED, LOGOUT, LOGIN_FAILED, LOGOUT_FAILED } from '../constants/user.constants';
+
+describe('user reducer', () => {
+  it('should return the initial state', () => {
+    expect(reducer(undefined, {})).toEqual(
+      {
+        username: '',
+        password: '',
+        submitted: false,
+        loginSucceeded: false,
+        loginFailed: false,
+        logoutSucceeded: false,
+        logoutFailed: false
+      }
+    )
+  })
+
+  it('should handle LOGIN_REQUESTED', () => {
+    expect(
+      reducer({}, {
+        type: LOGIN_REQUESTED,
+      })
+    ).toEqual(
+        {
+            submitted: true
+        }
+    )
+  });
+
+  it('should handle LOGIN', () => {
+    expect(
+      reducer({}, {
+        type: LOGIN,
+        username: 'Luke Skywalker',
+        password: '19BBY',
+        submitted: true,
+        loginSucceeded: true,
+        loginFailed: false
+      })
+    ).toEqual(
+        {
+            username: 'Luke Skywalker',
+            password: '19BBY',
+            submitted: true,
+            loginSucceeded: true,
+            loginFailed: false
+        }
+    )
+  });
+
+  it('should handle LOGIN_FAILED', () => {
+    expect(
+      reducer({}, {
+        type: LOGIN_FAILED,
+        submitted: true,
+        loginSucceeded: false,
+        loginFailed: true
+      })
+    ).toEqual(
+        {
+            submitted: true,
+            loginSucceeded: false,
+            loginFailed: true
+        }
+    )
+  });
+
+  it('should handle LOGOUT_REQUESTED', () => {
+    expect(
+      reducer({}, {
+        type: LOGOUT_REQUESTED,
+      })
+    ).toEqual(
+        {
+            
+        }
+    )
+  });
+
+  it('should handle LOGOUT', () => {
+    expect(
+      reducer({}, {
+        type: LOGOUT,
+        logoutSucceeded: true,
+        logoutFailed: false
+      })
+    ).toEqual(
+        {
+            logoutSucceeded: true,
+            logoutFailed: false
+        }
+    )
+  });
+
+  it('should handle LOGOUT_FAILED', () => {
+    expect(
+      reducer({}, {
+        type: LOGOUT_FAILED,
+        logoutSucceeded: false,
+        logoutFailed: true
+      })
+    ).toEqual(
+        {
+            logoutSucceeded: false,
+            logoutFailed: true
+        }
+    )
+  });
+
+});
